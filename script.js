36da5633b19b22923ddcab4b103f99df8a7def46
Naveen Kolambage <naveenkolambage@gmail.com>
2024-02-15 20:02:02 +1300

    added clock html-css

--- script.js
+++ script.js
@@ -11,15 +11,4 @@
 //     .join("");
 // });
 
-const labels = document.querySelectorAll(".form-control label");
-
-labels.forEach((label) => {
-  label.innerHTML = label.innerText
-    .split("")
-    .map(
-      (letter, idx) => `<span 
-    style="transition-delay:${idx * 50}ms"
-    >${letter}</span>`
-    )
-    .join("");
-});
+ 
